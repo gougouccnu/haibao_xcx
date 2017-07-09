4e3fb3c39517220accb017cd8401a67a0e1ba294
gougouccnu <lishaoweiccnu@163.com>
2017-07-09 09:35:28 +0800

    audio play ok with wx.createaudiocontext

--- pages/buyone/buyone.js
+++ pages/buyone/buyone.js
@@ -27,7 +27,14 @@ function caculateTotalPrice(orderList) {
 }
 
 Page(Object.assign({}, Zan.Quantity, Zan.Toast, {
+  onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+    this.audioCtx.setSrc('https://44480041.qcloud.la/user-4136aa7e.mp3')
+    this.audioCtx.play()
+  },
   data: {
+    src: '',
     audioAction: {
       method: 'pause'
     },
@@ -54,6 +61,19 @@ Page(Object.assign({}, Zan.Quantity, Zan.Toast, {
       min: 1,
       max: 99
     }
+  },
+  audioPlay: function () {
+    this.audioCtx.setSrc ('http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46')
+    this.audioCtx.play()
+  },
+  audioPause: function () {
+    this.audioCtx.pause()
+  },
+  audio14: function () {
+    this.audioCtx.seek(14)
+  },
+  audioStart: function () {
+    this.audioCtx.seek(0)
   },
   showToast() {
     this.showZanToast('微信支付');
@@ -126,6 +146,26 @@ Page(Object.assign({}, Zan.Quantity, Zan.Toast, {
       complete: () => {
       }
     });
+
+    wx.downloadFile({
+      url: 'https://44480041.qcloud.la/user-4136aa7e.mp3', //仅为示例，并非真实的资源
+      //url: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      success: function (res) {
+        console.log('down ok: ' + res.tempFilePath);
+        wx.playVoice({
+          filePath: res.tempFilePath,
+          fail: function(e) {
+            console.log(e);
+          },
+          complete: function(c) {
+            console.log('play voice complete');
+          }
+        })
+      },
+      fail:function (e) {
+        console.log(e);
+      }
+    })
 
   },
 
